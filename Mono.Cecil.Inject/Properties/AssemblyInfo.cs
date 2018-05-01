06d75812eca0357ba07b3d91678eb9c339df9f1f
denikson <geoffrey.hoooooorse@gmail.com>
2018-05-02 00:12:09 +0300

    Add support for Cecil 0.10

--- Mono.Cecil.Inject/Properties/AssemblyInfo.cs
+++ Mono.Cecil.Inject/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.6")]
-[assembly: AssemblyFileVersion("1.2.6")]
+[assembly: AssemblyVersion("1.2.7")]
+[assembly: AssemblyFileVersion("1.2.7")]
